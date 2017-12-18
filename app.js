d80fe16c7095d07b9835c1e27ae69ec5ed39b38a
soulriderx <writerfrenzy81@gmail.com>
2017-12-18 14:21:51 -0800

    added array of images

--- app.js
+++ app.js
@@ -12,3 +12,5 @@ function BusMall(name, filepath, numshown, numclicked) {
   this.numclicked = numclicked;
   BusMall.allBusMalls.push(this);
 }
+
+var pics = ['./images/bag.jpg', './images/bananas.jpg', './images/bathroom.jpg', './images/boots.jpg', './images/breakfast.jpg', './images/bubblegum.jpg', './images/chair.jpg', './images/cthulhu.jpg', './images/dog-duck.jpg', './images/dragon.jpg', './images/pen.jpg', './images/pet-sweep.jpg', './images/scissors.jpg', './images/shark.jpg', './images/sweep.png', './images/tauntaun.jpg', './images/unicorn.jpg', './images/usb.gif', './images/water-can.jpg', './images/wine-glass.jpg'];
