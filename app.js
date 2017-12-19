9f765b0dbf224d1d6e3b9fa42bedcab6ecd19a1a
soulriderx <writerfrenzy81@gmail.com>
2017-12-19 12:15:00 -0800

    small changes

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 'use strict';
 // better pattern
 
-
-
 BusMall.allBusMalls = [];
 
 function BusMall(name, filepath, numshown, numclicked) {
@@ -13,14 +11,14 @@ function BusMall(name, filepath, numshown, numclicked) {
   BusMall.allBusMalls.push(this);
 }
 
-var pics = ['./images/bag.jpg', './images/bananas.jpg', './images/bathroom.jpg', './images/boots.jpg', './images/breakfast.jpg', './images/bubblegum.jpg', './images/chair.jpg', './images/cthulhu.jpg', './images/dog-duck.jpg', './images/dragon.jpg', './images/pen.jpg', './images/pet-sweep.jpg', './images/scissors.jpg', './images/shark.jpg', './images/sweep.png', './images/tauntaun.jpg', './images/unicorn.jpg', './images/usb.gif', './images/water-can.jpg', '/images/wine.jpg'];
+var pics = ['images/bag.jpg', 'images/bananas.jpg', 'images/bathroom.jpg', 'images/boots.jpg', 'images/breakfast.jpg', 'images/bubblegum.jpg', 'images/chair.jpg', 'images/cthulhu.jpg', 'images/dog-duck.jpg', 'images/dragon.jpg', 'images/pen.jpg', 'images/pet-sweep.jpg', 'images/scissors.jpg', 'images/shark.jpg', 'images/sweep.png', 'images/tauntaun.jpg', 'images/unicorn.jpg', 'images/usb.gif', 'images/water-can.jpg', 'images/wine.jpg'];
 
-// var first = getElementById('first', [0]);
-// var second = getElementById('second', [1]);
-// var third = getElementById('third', [2]);
+var first = document.getElementById('first', [0]);
+var second = document.getElementById('second', [1]);
+var third = document.getElementById('third', [2]);
 
-function randomPic () {
+function randomPic () { // from MDN
 var randomNumber = Math.floor(Math.random() * 10) + 1;
 var imgName = "img_" + randomNumber;
-document.getElementById("first").src= './images/bag.jpg + "/"' + imgName ;
+document.getElementById("first").src='images/bag.jpg';
 }
