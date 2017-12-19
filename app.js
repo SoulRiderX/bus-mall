35d95b24f44ff561da47a927bddfcd1df2c04d55
soulriderx <writerfrenzy81@gmail.com>
2017-12-18 17:08:52 -0800

    switched 'for loop' for function

--- app.js
+++ app.js
@@ -15,10 +15,12 @@ function BusMall(name, filepath, numshown, numclicked) {
 
 var pics = ['./images/bag.jpg', './images/bananas.jpg', './images/bathroom.jpg', './images/boots.jpg', './images/breakfast.jpg', './images/bubblegum.jpg', './images/chair.jpg', './images/cthulhu.jpg', './images/dog-duck.jpg', './images/dragon.jpg', './images/pen.jpg', './images/pet-sweep.jpg', './images/scissors.jpg', './images/shark.jpg', './images/sweep.png', './images/tauntaun.jpg', './images/unicorn.jpg', './images/usb.gif', './images/water-can.jpg', '/images/wine.jpg'];
 
-var first = getElementById('first', [0]);
-var second = getElementById('second', [1]);
-var third = getElementById('third', [2]);
+// var first = getElementById('first', [0]);
+// var second = getElementById('second', [1]);
+// var third = getElementById('third', [2]);
 
-for (var i = 0; i < pics.length; i++) {
-  
+function randomPic () {
+var randomNumber = Math.floor(Math.random() * 10) + 1;
+var imgName = "img_" + randomNumber;
+document.getElementById("imageid").src= './images/bag.jpg + "/"' + imgName ;
 }
